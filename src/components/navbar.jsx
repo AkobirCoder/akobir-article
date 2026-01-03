0c879e677ab3af51eadf2f5e411235d41117c593
AkobirCoder <akobir0308@gmail.com>
2026-01-03 17:25:33 +0500

    ./src/components/navbar.jsx was updated

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import { Logo } from './assets';
 
 const Navbar = () => {
     return (
@@ -7,17 +8,17 @@ const Navbar = () => {
             <div className='row d-flex flex-column flex-md-row align-items-center py-2'>
                 <div className='col-4'>
                     <Link className='px-2' to={'/'}>
-                        Navbar
+                        <img src={Logo} width={150} height={75} alt="Logo" />
                     </Link>
                 </div>
                 <nav className='col-8 py-2'>
                     <div className='row'>
                         <div className='col-6 d-flex justify-content-evenly'>
-                            <Link className=''>home</Link>
-                            <Link className=''>home</Link>
-                            <Link className=''>home</Link>
-                            <Link className=''>home</Link>
-                            <Link className=''>home</Link>
+                            <Link className='text-decoration-none'>home</Link>
+                            <Link className='text-decoration-none'>home</Link>
+                            <Link className='text-decoration-none'>home</Link>
+                            <Link className='text-decoration-none'>home</Link>
+                            <Link className='text-decoration-none'>home</Link>
                         </div>
                         <div className='col-6 d-flex justify-content-end'>
                             <Link to={'/login'} className='me-3 px-2 text-dark text-decoration-none'>
