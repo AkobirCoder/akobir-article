43fac72dcde1543c82c1378ff272552ad8f61bc1
AkobirCoder <akobir0308@gmail.com>
2026-01-03 23:23:39 +0500

    ./src/constants/index.js was updated

---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const navigationLinks = [
+    {
+        id: 1,
+        title: 'About us',
+        path: '/about-us',
+    },
+    {
+        id: 2,
+        title: 'Articles',
+        path: '/articles',
+    },
+    {
+        id: 3,
+        title: 'Blogs',
+        path: '/blogs',
+    },
+    {
+        id: 4,
+        title: 'Contact us',
+        path: '/contact-us',
+    },
+];
