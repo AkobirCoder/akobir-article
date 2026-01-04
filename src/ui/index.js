1d1e79e3edcf448be438020d73db8080bd4183b0
AkobirCoder <akobir0308@gmail.com>
2026-01-04 17:57:41 +0500

    ./src/ui/index.js was added

---
+++ src/ui/index.js
@@ -0,0 +1,47 @@
+export {default as Input} from './input';
+
+export const loginInputProps = [
+    {
+        id: 'floatingEmail',
+        type: 'email',
+        name: 'email',
+        completeName: 'email',
+        placeholder: 'name@example.com',
+        label: 'Email address',
+    },
+    {
+        id: 'floatingPassword',
+        type: 'password',
+        name: 'password',
+        completeName: 'password',
+        placeholder: 'Password',
+        label: 'Password',
+    }
+];
+
+export const registerInputProps = [
+    {
+        id: 'floatingUsername',
+        type: 'text',
+        name: 'username',
+        completeName: 'username',
+        placeholder: 'username',
+        label: 'Username',
+    },
+    {
+        id: 'floatingEmail',
+        type: 'email',
+        name: 'email',
+        completeName: 'email',
+        placeholder: 'name@example.com',
+        label: 'Email address',
+    },
+    {
+        id: 'floatingPassword',
+        type: 'password',
+        name: 'password',
+        completeName: 'password',
+        placeholder: 'Password',
+        label: 'Password',
+    }
+];
