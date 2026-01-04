645fe1e8b0e383253f5fc2a74283ebc3bd35c5a4
AkobirCoder <akobir0308@gmail.com>
2026-01-04 12:27:19 +0500

    ./src/App.jsx was updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
-import { AboutUs, Articles, Blogs, ContactUs, Login, Main, Navbar, Register } from './components';
+import { AboutUs, Articles, Blogs, ContactUs, Login, Main, Navbar, Register, Sidebar } from './components';
 
 const App = () => {
     return (
@@ -8,15 +8,22 @@ const App = () => {
             <div className='border-bottom bg-body-tertiary'>
                 <Navbar />
             </div>
-            <Routes>
-                <Route path='/' element={<Main />} />
-                <Route path='/about-us' element={<AboutUs />} />
-                <Route path='/articles' element={<Articles />} />
-                <Route path='/blogs' element={<Blogs />} />
-                <Route path='/contact-us' element={<ContactUs />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/register' element={<Register />} />
-            </Routes>
+            <div className='row g-0'>
+                {/* <div style={{height: '89.7vh'}} className='d-none d-md-block col-md-2 bg-dark-subtle'>
+                    <Sidebar />
+                </div> */}
+                <div style={{height: '89.7vh'}} className='col-12 col-md-10'>
+                    <Routes>
+                        <Route path='/' element={<Main />} />
+                        <Route path='/about-us' element={<AboutUs />} />
+                        <Route path='/articles' element={<Articles />} />
+                        <Route path='/blogs' element={<Blogs />} />
+                        <Route path='/contact-us' element={<ContactUs />} />
+                        <Route path='/login' element={<Login />} />
+                        <Route path='/register' element={<Register />} />
+                    </Routes>
+                </div>
+            </div>
         </div>
     );
 }
