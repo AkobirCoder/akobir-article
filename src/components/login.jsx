c85750e513964e0d6544a7852cf5b3b109302fa1
AkobirCoder <akobir0308@gmail.com>
2026-01-06 23:49:47 +0500

    ./src/components/login.jsx was updated

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { FormLogo } from './assets';
 import { Input, loginInputProps } from '../ui';
+import { useDispatch, useSelector } from 'react-redux';
+import { loginUserStart } from '../slice/auth';
 
 const Login = () => {
     const [formData, setFormData] = useState({
@@ -16,8 +18,14 @@ const Login = () => {
         });
     }
 
-    const resetForm = (event) => {
+    const dispatch = useDispatch();
+
+    const {isLoading} = useSelector((state) => state.auth);
+
+    const loginHandler = (event) => {
         event.preventDefault();
+
+        dispatch(loginUserStart());
 
         setFormData({
             email: '',
@@ -28,7 +36,7 @@ const Login = () => {
     return (
         <div className='d-flex justify-content-center align-items-center h-100'>
             <main className='form-signin col-10 col-md-4'>
-                <form className='text-center mt-3 mt-md-0' onSubmit={resetForm}>
+                <form className='text-center mt-3 mt-md-0' onSubmit={loginHandler}>
                     <img className='mb-4' src={FormLogo} alt="Form logo" />
                     <h1 className='h3 mb-3 fw-normal'>Please sign in</h1>
                     
@@ -45,13 +53,21 @@ const Login = () => {
                         })
                     }
 
-                    {/* <div className='form-check mt-3 mb-3'>
-                        <label>
-                            <input type="checkbox" className='form-check-input' value={'remember-me'} /> Remember me
-                        </label>
-                    </div> */}
-
-                    <button type='submit' className='w-100 btn btn-lg btn-primary'>Sign in</button>
+                    <button 
+                        type='submit' 
+                        className='w-100 btn btn-lg btn-primary' 
+                        disabled={isLoading}
+                    >
+                        {
+                            (() => {
+                                if (isLoading) {
+                                    return 'Loading...';
+                                } else {
+                                    return 'Sign in';
+                                }
+                            })()
+                        }    
+                    </button>
                     <p className='mt-3 mt-md-5 mb-3 text-muted'>© 2025-2026</p>
                 </form>
             </main>
