d3ca518866a758d5a68dd42b18f767d7c8c6697f
AkobirCoder <akobir0308@gmail.com>
2026-01-06 23:50:23 +0500

    ./src/slice/auth.js was added

---
+++ src/slice/auth.js
@@ -0,0 +1,23 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+    isLoading: false,
+    loggedIn: false,
+    user: null,
+}
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState: initialState,
+    reducers: {
+        loginUserStart: (state) => {
+            state.isLoading = true;
+        },
+        loginUserSuccess: () => {},
+        loginUserFailure: () => {},
+    }
+});
+
+export const {loginUserStart} = authSlice.actions;
+
+export default authSlice.reducer;
