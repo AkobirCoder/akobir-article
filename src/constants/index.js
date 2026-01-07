36bf6e234a631691d6a150925fb1c15ffab557f4
AkobirCoder <akobir0308@gmail.com>
2026-01-07 16:23:04 +0500

    ./src/constants/index.js was updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -25,3 +25,62 @@ export const navigationLinks = [
         path: '/contact-us',
     },
 ];
+
+export const sidebarItems = [
+    {
+        id: 1,
+        title: 'item1',
+    },
+    {
+        id: 2,
+        title: 'item2',
+    },
+    {
+        id: 3,
+        title: 'item3',
+    },
+    {
+        id: 4,
+        title: 'item4',
+    },
+    {
+        id: 5,
+        title: 'item5',
+    },
+    {
+        id: 6,
+        title: 'item6',
+    },
+    {
+        id: 7,
+        title: 'item7',
+    },
+    {
+        id: 8,
+        title: 'item8',
+    },
+    {
+        id: 9,
+        title: 'item9',
+    },
+    {
+        id: 10,
+        title: 'item10',
+    },
+    {
+        id: 11,
+        title: 'item11',
+    },
+    {
+        id: 12,
+        title: 'item12',
+    },
+    {
+        id: 13,
+        title: 'item13',
+    },
+    {
+        id: 14,
+        title: 'item14',
+    },
+]
