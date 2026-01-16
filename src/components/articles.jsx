af900b2fa887609594abe7c0e8579c6b7f7f5e91
AkobirCoder <akobir0308@gmail.com>
2026-01-17 00:10:46 +0500

    ./src/components/articles.jsx was updated

--- src/components/articles.jsx
+++ src/components/articles.jsx
@@ -1,21 +1,40 @@
 import React from 'react';
 import ArticleCard from './article-card';
 import { useSelector } from 'react-redux';
+import { Loader } from '../ui';
 
 const Articles = () => {
-    const {articles} = useSelector((state) => state.article)
+    const {articles, isLoading} = useSelector((state) => state.article);
 
     return (
         <div className='album pb-3'>
-            <div className='row row-cols-1 row-cols-md-3 g-3'>
-                {
-                    articles.map((item) => {
-                        return (
-                            <ArticleCard key={item.id} {...item} />
-                        );
-                    })
-                }
-            </div>
+            <>
+                {(
+                    () => {
+                        if (isLoading) {
+                            return (
+                                <div className='d-flex align-items-center justify-content-center'>
+                                    <Loader />
+                                </div>
+                            );
+                        } else {
+                            return (
+                                <div className='row row-cols-1 row-cols-md-3 g-3'>
+                                    {
+                                        articles.map((item) => {
+                                            return (
+                                                <ArticleCard key={item.id} {...item} />
+                                            );
+                                        })
+                                    }
+                                </div>
+                            );
+                        }
+                    }
+                )()}
+            </>
+            
+            
         </div>
     );
 }
