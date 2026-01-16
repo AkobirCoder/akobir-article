97b05fc8f4b046287c160acd4aad2c0f572d4d12
AkobirCoder <akobir0308@gmail.com>
2026-01-16 23:49:36 +0500

    ./src/service/article.js was added

---
+++ src/service/article.js
@@ -0,0 +1,11 @@
+import axios from './api';
+
+const ArticleService = {
+    async getArticles() {
+        const response = await axios.get('/articles');
+
+        return response.data;
+    }
+}
+
+export default ArticleService;
