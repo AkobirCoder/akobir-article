4fcc6073ae12fb0c678822cdea03be5df9b366d9
AkobirCoder <akobir0308@gmail.com>
2026-01-16 23:46:06 +0500

    ./src/components/articles.jsx was updated

--- src/components/articles.jsx
+++ src/components/articles.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import ArticleCard from './article-card';
+import { useSelector } from 'react-redux';
 
 const Articles = () => {
+    const {articles} = useSelector((state) => state.article)
+
     return (
-        <div>Articles</div>
+        <div className='album pb-3'>
+            <div className='row row-cols-1 row-cols-md-3 g-3'>
+                {
+                    articles.map((item) => {
+                        return (
+                            <ArticleCard key={item.id} {...item} />
+                        );
+                    })
+                }
+            </div>
+        </div>
     );
 }
 
