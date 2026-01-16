f0d49495f58c89fd00184a7a5be3beda00450574
AkobirCoder <akobir0308@gmail.com>
2026-01-17 01:16:50 +0500

    ./src/components/articles.jsx was updated

--- src/components/articles.jsx
+++ src/components/articles.jsx
@@ -2,40 +2,45 @@ import React from 'react';
 import ArticleCard from './article-card';
 import { useSelector } from 'react-redux';
 import { Loader } from '../ui';
+import { useNavigate } from 'react-router-dom';
 
 const Articles = () => {
     const {articles, isLoading} = useSelector((state) => state.article);
 
+    const navigate = useNavigate();
+
+    const navigateHandler = (item) => {
+        navigate(`/article/${item}`);
+    }
+
     return (
-        <div className='album pb-3'>
-            <>
-                {(
-                    () => {
-                        if (isLoading) {
-                            return (
-                                <div className='d-flex align-items-center justify-content-center'>
-                                    <Loader />
-                                </div>
-                            );
-                        } else {
-                            return (
+        <>
+            {(
+                () => {
+                    if (isLoading) {
+                        return (
+                            <div className='d-flex align-items-center justify-content-center h-100'>
+                                <Loader />
+                            </div>
+                        );
+                    } else {
+                        return (
+                            <div className='album pb-3'>
                                 <div className='row row-cols-1 row-cols-md-3 g-3'>
                                     {
                                         articles.map((item) => {
                                             return (
-                                                <ArticleCard key={item.id} {...item} />
+                                                <ArticleCard key={item.id} {...item} navigateHandler={navigateHandler} />
                                             );
                                         })
                                     }
                                 </div>
-                            );
-                        }
+                            </div>
+                        );
                     }
-                )()}
-            </>
-            
-            
-        </div>
+                }
+            )()}
+        </>
     );
 }
 
