3249162c571b6461d2ac93f4f45120cadc5712fd
AkobirCoder <akobir0308@gmail.com>
2026-01-18 02:02:48 +0500

    ./src/components/create-article-form.jsx was updated

--- src/components/create-article-form.jsx
+++ src/components/create-article-form.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Input, Textarea } from '../ui';
 
-const CreateArticleForm = () => {
+const CreateArticleForm = ({formData, changeHandlerInput}) => {
     return (
-        <div>CreateArticleForm</div>
+        <form>
+            <Input label={'Title'} placeholder={'Title'} name={'title'} value={formData.title} changeHandlerInput={changeHandlerInput} />
+            <Textarea label={'Description'} placeholder={'Description'} name={'description'} value={formData.description} changeHandlerInput={changeHandlerInput} />
+            <Textarea label={'Body'} placeholder={'Body'} name={'body'} value={formData.body} changeHandlerInput={changeHandlerInput} height={'250px'} />
+            <button type='submit' className='w-100 btn btn-lg btn-primary'>Create</button>
+        </form>
     );
 }
 
