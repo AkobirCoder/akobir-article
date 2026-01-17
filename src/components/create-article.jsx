5992a06079f51d824df2aff15a309f445c1c39f4
AkobirCoder <akobir0308@gmail.com>
2026-01-18 02:03:01 +0500

    ./src/components/create-article.jsx was updated

--- src/components/create-article.jsx
+++ src/components/create-article.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Input, Textarea } from '../ui';
+// import { Input, Textarea } from '../ui';
+import { CreateArticleForm } from './index';
 
 const CreateArticle = () => {
     const [formData, setFormData] = useState({
@@ -20,12 +21,7 @@ const CreateArticle = () => {
         <div className='row d-flex align-items-center justify-content-center' style={{minHeight: '100%'}}>
             <div className='col-11 col-md-8'>
                 <h1 className='fs-2 text-center'>Create article</h1>
-                <form>
-                    <Input label={'Title'} placeholder={'Title'} name={'title'} value={formData.title} changeHandlerInput={changeHandlerInput} />
-                    <Textarea label={'Description'} placeholder={'Description'} name={'description'} value={formData.description} changeHandlerInput={changeHandlerInput} />
-                    <Textarea label={'Body'} placeholder={'Body'} name={'body'} value={formData.body} changeHandlerInput={changeHandlerInput} height={'250px'} />
-                    <button type='submit' className='w-100 btn btn-lg btn-primary'>Create</button>
-                </form>
+                <CreateArticleForm formData={formData} changeHandlerInput={changeHandlerInput} />
             </div>
         </div>
     );
