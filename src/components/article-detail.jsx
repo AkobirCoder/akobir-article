900e8e044a72ab4cc1dc57102949c1c557da5923
AkobirCoder <akobir0308@gmail.com>
2026-01-17 11:22:38 +0500

    ./src/components/article-detail.jsx was updated

--- src/components/article-detail.jsx
+++ src/components/article-detail.jsx
@@ -43,7 +43,7 @@ const ArticleDetail = () => {
                         );
                     }  else {
                         return (
-                            <div className="p-5 mb-4 bg-body-tertiary rounded-3"> 
+                            <div className="p-5 bg-body-tertiary rounded-3"> 
                                 <div className="container-fluid py-4"> 
                                     <h1 className="display-5 fw-semibold">{articleDetail.title}</h1> 
                                     <p className="col-md-8 fs-5">{articleDetail.description}</p>
