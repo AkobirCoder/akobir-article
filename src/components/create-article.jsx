9ddfeebc95465b0025b147f7a10f2e42bf170891
AkobirCoder <akobir0308@gmail.com>
2026-01-20 00:47:14 +0500

    ./src/components/create-article.jsx was updated

--- src/components/create-article.jsx
+++ src/components/create-article.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-// import { Input, Textarea } from '../ui';
 import { CreateArticleForm } from './index';
+import ArticleService from '../service/article';
+import { useDispatch } from 'react-redux';
+import { postArticleFailure, postArticleStart, postArticleSuccess } from '../slice/article';
 
 const CreateArticle = () => {
     const [formData, setFormData] = useState({
@@ -17,11 +19,37 @@ const CreateArticle = () => {
         });
     }
 
+    const dispatch = useDispatch();
+
+    const formSubmit = async (event) => {
+        event.preventDefault();
+
+        dispatch(postArticleStart());
+
+        const {title, description, body} = formData;
+
+        const article = {title, description, body}
+
+        try {
+            const response = await ArticleService.postArticle(article);
+
+            // console.log(response);
+
+            dispatch(postArticleSuccess(response));
+        } catch (error) {
+            dispatch(postArticleFailure());
+        }
+    }
+
     return (
         <div className='row d-flex align-items-center justify-content-center' style={{minHeight: '100%'}}>
             <div className='col-11 col-md-8'>
                 <h1 className='fs-2 text-center'>Create article</h1>
-                <CreateArticleForm formData={formData} changeHandlerInput={changeHandlerInput} />
+                <CreateArticleForm 
+                    formData={formData} 
+                    changeHandlerInput={changeHandlerInput} 
+                    formSubmit={formSubmit} 
+                />
             </div>
         </div>
     );
