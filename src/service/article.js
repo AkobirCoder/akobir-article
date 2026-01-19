4ca8164d7ff867d1d6fe3524201a256fb4bd5a03
AkobirCoder <akobir0308@gmail.com>
2026-01-20 00:47:46 +0500

    ./src/service/article.js was updated

--- src/service/article.js
+++ src/service/article.js
@@ -11,6 +11,12 @@ const ArticleService = {
         const response = await axios.get(`/articles/${slug}`);
 
         return response.data;
+    },
+
+    async postArticle(article) {
+        const response = await axios.post('/articles', {article});
+
+        return response.data;
     }
 }
 
