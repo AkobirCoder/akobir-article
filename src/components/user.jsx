4defc439dcbbf3fa6432a8f1780513f25b485f8b
AkobirCoder <akobir0308@gmail.com>
2026-01-26 01:25:00 +0500

    ./src/components/user.jsx was updated

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Loader } from '../ui';
+import { userDetailFailure, userDetailStart, userDetailSuccess } from '../slice/auth';
+import AuthService from '../service/auth';
+import { getItem } from '../helpers/persistance-storage';
+import { useNavigate } from 'react-router-dom';
 
 const User = () => {
+    const dispatch = useDispatch();
+
+    const {isLoading, user} = useSelector((state) => state.auth);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const token = getItem('token');
+
+        if (!token) {
+            navigate('/login');
+        }
+
+        const getUserProfile = async () => {
+            dispatch(userDetailStart());
+
+            try {
+                const response = await AuthService.getUser();
+
+                dispatch(userDetailSuccess(response.user));
+            } catch (error) {
+                dispatch(userDetailFailure(error.response.data.errors));
+            }
+        }
+
+        getUserProfile();
+    }, [dispatch, navigate]);
+
     return (
-        <div>User</div>
-    )
+        <>
+            {(
+                () => {
+                    if (!user) {
+                        return (
+                            <div className='d-flex align-items-center justify-content-center h-100'>
+                                <Loader />
+                            </div>
+                        );
+                    } else {
+                        return (
+                            <div>
+                                <div>{user.username}</div>
+                                <button type='button' disabled={isLoading} className='btn btn-success'>Save</button>
+                            </div>
+                        );
+                    }
+                }
+            )()}
+        </>
+    );
 }
 
-export default User
+export default User;
