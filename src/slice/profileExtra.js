4c45d781e633f3d5429f544640807ccbc66d0127
AkobirCoder <akobir0308@gmail.com>
2026-01-27 20:22:17 +0500

    ./src/slice/profileExtra.js was added

---
+++ src/slice/profileExtra.js
@@ -0,0 +1,47 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+    profileExtra: {
+        birthYear: '',
+        phone: '',
+        field: '',
+        description: '',
+        study: '',
+        socials: {
+            telegram: '',
+            instagram: '',
+            linkedin: '',
+        }
+    },
+    isSaving: false,
+    error: null,
+}
+
+export const profileExtraSlice = createSlice({
+    name: 'profileExtra',
+    initialState: initialState,
+    reducers: {
+        saveProfileExtraStart: (state) => {
+            state.isSaving = true;
+        },
+        saveprofileExtraSuccess: (state, action) => {
+            state.isSaving = false;
+            state.profileExtra = {
+                ...state.profileExtra,
+                ...action.payload,
+                socials: {
+                    ...state.profileExtra.socials,
+                    ...action.payload.socials
+                },
+            };
+        },
+        saveProfileExtraFailure: (state, action) => {
+            state.isSaving = false;
+            state.error = action.payload;
+        }
+    }
+});
+
+export const {saveProfileExtraStart, saveprofileExtraSuccess, saveProfileExtraFailure} = profileExtraSlice.actions;
+
+export default profileExtraSlice.reducer;
