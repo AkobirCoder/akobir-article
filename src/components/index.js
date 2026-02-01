16efd5442fd49525be51a74aeb560fb821c28f73
AkobirCoder <akobir0308@gmail.com>
2026-02-02 00:14:27 +0500

    ./src/components/index.js was updated

--- src/components/index.js
+++ src/components/index.js
@@ -15,4 +15,5 @@ export {default as Login} from './login';
 export {default as Register} from './register';
 export {default as User} from './user';
 export {default as UserEdit} from './user-edit';
+export {default as UserLogout} from './user-logout';
 export {default as ValidationError} from './validation-error';
