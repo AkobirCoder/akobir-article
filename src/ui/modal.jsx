f0e53e77983dfa2b2a7dd172b9d7e6031ae30b00
AkobirCoder <akobir0308@gmail.com>
2026-02-02 12:07:41 +0500

    ./src/ui/modal.jsx was updated

--- src/ui/modal.jsx
+++ src/ui/modal.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
+import {createPortal} from 'react-dom';
 
-const Modal = () => {
-    return (
-        <div>Modal</div>
+const Modal = ({btnName, onClose, logoutHandler}) => {
+    return createPortal(
+        <>
+            <div className="modal-backdrop fade show"></div>
+            <div className="modal fade show d-block" tabIndex="-1">
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Confirm logout</h5>
+                            <button
+                                type="button"
+                                className="btn-close"
+                                onClick={onClose}
+                            />
+                        </div>
+                        <div className="modal-body">
+                            Are you sure you want to logout?
+                        </div>
+                        <div className="modal-footer">
+                            <button
+                                type="button"
+                                className="btn btn-secondary"
+                                onClick={onClose}
+                            >
+                                Cancel
+                            </button>
+                            <button
+                                type="button"
+                                className="btn btn-outline-danger"
+                                onClick={logoutHandler}
+                            >
+                                {btnName}
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>,
+        document.body
     );
 }
 
