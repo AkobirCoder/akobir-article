dc95c3f50afcaf0081bacdff47289d33ad5a7129
AkobirCoder <akobir0308@gmail.com>
2026-02-03 05:04:02 +0500

    ./src/App.jsx was updated

--- src/App.jsx
+++ src/App.jsx
@@ -71,39 +71,49 @@ const App = () => {
                 <Navbar />
             </div>
             <div className='row g-0'>
-                <div 
-                    style={{
-                        height: 'calc(100vh - 74px)', 
-                        marginTop: 74,
-                        transform: toggleSidebar 
-                        ? 'translateX(calc(-100% + 68px))' 
-                        : 'translateX(0)', 
-                        transition: 'transform 0.3s ease'
-                    }} 
-                    className='position-fixed top-0 bottom-0 z-3 d-none d-md-block p-3 col-md-2 bg-light border-end'
-                >
-                    <div className='row g-0 d-flex align-items-start'>
-                        <div className={`${toggleSidebar ? 'col-9' : 'col-10'}`}>
-                            <Sidebar />
-                        </div>
-                        <div className={`
-                            d-flex 
-                            ${toggleSidebar ? 'justify-content-end col-3' : 'justify-content-center col-2'}
-                        `}>
-                            <button
-                                type='button'
-                                className='btn bg-primary-subtle px-2 py-0 fs-5 border-primary text-primary pointer'
-                                onClick={sidebarHandler}
-                            >
-                                {
-                                    toggleSidebar
-                                        ? <i className="bi bi-arrow-bar-right"></i>
-                                        : <i className="bi bi-arrow-bar-left"></i>
-                                }
-                            </button>
-                        </div>
-                    </div>
-                </div>
+                {(
+                    () => {
+                        if (!user) {
+                            return '';
+                        } else {
+                            return (
+                                <div 
+                                    style={{
+                                        height: 'calc(100vh - 74px)', 
+                                        marginTop: 74,
+                                        transform: toggleSidebar 
+                                        ? 'translateX(calc(-100% + 68px))' 
+                                        : 'translateX(0)', 
+                                        transition: 'transform 0.3s ease'
+                                    }} 
+                                    className='position-fixed top-0 bottom-0 z-3 d-none d-md-block p-3 col-md-2 bg-light border-end'
+                                >
+                                    <div className='row g-0 d-flex align-items-start'>
+                                        <div className={`${toggleSidebar ? 'col-9' : 'col-10'}`}>
+                                            <Sidebar />
+                                        </div>
+                                        <div className={`
+                                            d-flex 
+                                            ${toggleSidebar ? 'justify-content-end col-3' : 'justify-content-center col-2'}
+                                        `}>
+                                            <button
+                                                type='button'
+                                                className='btn bg-primary-subtle px-2 py-0 fs-5 border-primary text-primary pointer'
+                                                onClick={sidebarHandler}
+                                            >
+                                                {
+                                                    toggleSidebar
+                                                        ? <i className="bi bi-arrow-bar-right"></i>
+                                                        : <i className="bi bi-arrow-bar-left"></i>
+                                                }
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            );
+                        }
+                    }
+                )()}
                 <div 
                     style={{
                         height: 'calc(100vh - 74px)', 
@@ -113,7 +123,7 @@ const App = () => {
                     }} 
                     className={`
                         position-absolute end-0 z-1 p-3 
-                        col-12 col-md-10 
+                        ${user ? 'col-12 col-md-10' : 'col-md-12'} 
                         ${toggleSidebar ? 'col-md-12 sidebar' : 'col-md-10'}
                         main-content
                     `}
