348953300fe27524bea2590b9eb8554207256d05
AkobirCoder <akobir0308@gmail.com>
2026-02-16 20:42:54 +0500

    ./src/components/sidebar-menu.jsx was added

---
+++ src/components/sidebar-menu.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { sidebarMenuItems } from '../constants';
+import { SidebarMenuItem } from './index';
+
+const SidebarMenu = () => {
+    return (
+        <div>
+            <h2 className='h4 mb-3'>Catigories</h2>
+            <div className='btn-group-vertical col-12'>
+                {
+                    sidebarMenuItems.map((item, index) => {
+                        return (
+                            <SidebarMenuItem key={item.id} title={item.title} index={index} />
+                        );
+                    })
+                }
+            </div>
+        </div>
+    );
+}
+
+export default SidebarMenu;
