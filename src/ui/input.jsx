062abc1424051ca2c44ed2a9655f297c6870f470
AkobirCoder <akobir0308@gmail.com>
2026-02-16 23:47:52 +0500

    ./src/ui/input.jsx was updated

--- src/ui/input.jsx
+++ src/ui/input.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 
-const Input = ({id, type, name, completeName, placeholder, margin, label, value, changeHandlerInput}) => {
+const Input = ({mark, id, type, name, completeName, placeholder, margin, label, value, changeHandlerInput}) => {
     return (
-        <div className='form-floating'>
+        <div 
+            className={`form-floating ${(
+                () => {
+                    switch (mark) {
+                        case 'user-form-input':
+                            return 'col-12 col-md-6 mb-2 mb-md-0';
+                    
+                        case 'article-form-input':
+                            return 'mb-2';
+
+                        case 'sign-up-form-input':
+                            return 'mb-2';
+                        default:
+                            return;
+                    }
+                }
+            )()}`}
+        >
             <input 
                 type={type} 
                 name={name}
