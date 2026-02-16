ae9bd45c206d3efed0fffd65b64f0de772778139
AkobirCoder <akobir0308@gmail.com>
2026-02-16 23:44:56 +0500

    ./src/components/article-detail.jsx was updated

--- src/components/article-detail.jsx
+++ src/components/article-detail.jsx
@@ -1,17 +1,22 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {useParams} from 'react-router-dom';
+import {useNavigate, useParams} from 'react-router-dom';
 import moment from 'moment';
 import { getArticleDetailFailure, getArticleDetailStart, getArticleDetailSuccess } from '../slice/article';
 import ArticleService from '../service/article';
 import { Loader } from '../ui';
+import { ArrowToRight } from '@boxicons/react';
 
 const ArticleDetail = () => {
     const {slug} = useParams();
 
     const dispatch = useDispatch();
 
+    const {loggedIn} = useSelector((state) => state.auth);
+
     const {articleDetail} = useSelector((state) => state.article);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
         const getArticleDetail = async () => {
@@ -31,6 +36,18 @@ const ArticleDetail = () => {
         getArticleDetail();
     }, [slug, dispatch]);
 
+    const navigateArticles = () => {
+        navigate('/articles');
+    }
+
+    const navigateUserArticles = () => {
+        if (loggedIn) {
+            navigate('/my-articles');
+        } else {
+            navigate('/login');
+        }
+    }
+
     return (
         <>
             {(
@@ -46,7 +63,7 @@ const ArticleDetail = () => {
                             <div className="p-3 p-md-5 bg-body-tertiary rounded-3"> 
                                 <div className="container-fluid py-3 py-md-4"> 
                                     <h1 className="display-md-5 fw-semibold">{articleDetail.title}</h1> 
-                                    <p className="col-md-8 fs-md-5 mb-0 mb-mb-3">{articleDetail.description}</p>
+                                    <p className="col-md-8 fs-md-5 mb-0 mb-md-3">{articleDetail.description}</p>
                                     <div className='d-flex justify-content-start gap-5 mt-3 mt-md-4'>
                                         <div>
                                             <p className='text-muted'>
@@ -62,8 +79,8 @@ const ArticleDetail = () => {
                                             <p><span className='fw-semibold'>Author: </span>{articleDetail.author.username}</p>
                                         </div>
                                     </div>
-                                    <div className='col col-md-6'>
-                                        <div className='row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-lg h-md-250 position-relative'>
+                                    <div className='col col-md-8 d-flex'>
+                                        <div className='row g-0 border rounded overflow-hidden flex-column-reverse flex-md-row mb-4 shadow-lg h-md-250 position-relative'>
                                             <div className='col-12 col-sm-8 p-3 p-md-4 d-flex flex-column position-static'>
                                                 <strong className='d-inline-block mb-2 text-primary text-uppercase'>
                                                     {articleDetail.author.username}
@@ -97,8 +114,30 @@ const ArticleDetail = () => {
                                             </div>
                                         </div>
                                     </div>
-                                    <div>{articleDetail.body}</div>
-                                </div> 
+                                    <div style={{textAlign: 'justify'}}>{articleDetail.body}</div>
+                                    <div className='row mt-4 mt-md-5'>
+                                        <div className='col-12 col-md-3 mb-2 mb-md-0'>
+                                            <button 
+                                                style={{backgroundImage: 'var(--bs-gradient)'}}
+                                                className='btn btn-primary w-100' 
+                                                onClick={navigateArticles}
+                                            >
+                                                <span className='me-2'>Back to articles</span>
+                                                <ArrowToRight />
+                                            </button>
+                                        </div>
+                                        <div className='col-12 col-md-3'>
+                                            <button 
+                                                style={{backgroundImage: 'var(--bs-gradient)'}}
+                                                className='btn btn-secondary w-100' 
+                                                onClick={navigateUserArticles}
+                                            >
+                                                <span className='me-2'>Back to my articles</span>
+                                                <ArrowToRight />
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         );
                     }
