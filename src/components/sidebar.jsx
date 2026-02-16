b1308a0e017dd47fc1c84775556c7b9e33ac9c6a
AkobirCoder <akobir0308@gmail.com>
2026-02-16 20:43:25 +0500

    ./src/components/sidebar.jsx was updated

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,21 +1,53 @@
 import React from 'react';
-import { sidebarItems } from '../constants';
-import SidebarItem from './sidebar-item';
+import { SidebarMenu } from './index'
 
-const Sidebar = () => {
+const Sidebar = ({user, toggleSidebar, sidebarHandler}) => {
     return (
-        <div>
-            <h2 className='h4 mb-3'>Catigories</h2>
-            <div className='btn-group-vertical col-12'>
-                {
-                    sidebarItems.map((item, index) => {
+        <>
+            {(
+                () => {
+                    if (!user) {
+                        return '';
+                    } else {
                         return (
-                            <SidebarItem key={item.id} title={item.title} index={index} />
-                        )
-                    })
+                            <div 
+                                style={{
+                                    height: 'calc(100vh - 74px)', 
+                                    marginTop: 74,
+                                    transform: toggleSidebar 
+                                    ? 'translateX(calc(-100% + 68px))' 
+                                    : 'translateX(0)', 
+                                    transition: 'transform 0.3s ease'
+                                }} 
+                                className='position-fixed top-0 bottom-0 z-3 d-none d-md-block p-3 col-md-2 bg-light border-end'
+                            >
+                                <div className='row g-0 d-flex align-items-start'>
+                                    <div className={`${toggleSidebar ? 'col-9' : 'col-10'}`}>
+                                        <SidebarMenu />
+                                    </div>
+                                    <div className={`
+                                        d-flex 
+                                        ${toggleSidebar ? 'justify-content-end col-3' : 'justify-content-center col-2'}
+                                    `}>
+                                        <button
+                                            type='button'
+                                            className='btn bg-primary-subtle px-2 py-0 fs-5 border-primary text-primary pointer'
+                                            onClick={sidebarHandler}
+                                        >
+                                            {
+                                                toggleSidebar
+                                                    ? <i className="bi bi-arrow-bar-right"></i>
+                                                    : <i className="bi bi-arrow-bar-left"></i>
+                                            }
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    }
                 }
-            </div>
-        </div>
+            )()}
+        </>
     );
 }
 
