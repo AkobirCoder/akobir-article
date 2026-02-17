22be148c3278400c855c988657442787df2f38e1
AkobirCoder <akobir0308@gmail.com>
2026-02-17 17:13:26 +0500

    ./src/components/articles/articles.jsx was moved to articles

---
+++ src/components/articles/articles.jsx
@@ -0,0 +1,105 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { ArticleCard } from '../index';
+import { Loader } from '../../ui/index';
+import { 
+    deleteArticleFailure, 
+    deleteArticleStart, 
+    deleteArticleSuccess, 
+    getArticlesStart, 
+    getArticleSuccess 
+} from '../../slice/article';
+import ArticleService from '../../service/article';
+
+const Articles = () => {
+    const dispatch = useDispatch();
+
+    const {articles, isLoading} = useSelector((state) => state.article);
+
+    const navigate = useNavigate();
+
+    const navigateArticleViewHandler = (item) => {
+        navigate(`/view-article/${item}`);
+    }
+
+    const navigateArticleEditHandler = (item) => {
+        navigate(`/edit-article/${item}`);
+    }
+
+    const getArticles = async () => {
+        dispatch(getArticlesStart());
+
+        try {
+            const response = await ArticleService.getArticles();
+
+            dispatch(getArticleSuccess(response.articles));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const deleteArticle = async (slug) => {
+        dispatch(deleteArticleStart());
+
+        try {
+            const response = await ArticleService.deleteArticle(slug);
+
+            dispatch(deleteArticleSuccess(response));
+
+            getArticles();
+        } catch (error) {
+            dispatch(deleteArticleFailure());
+
+            // console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getArticles();
+    }, []);
+
+    return (
+        <>
+            {(
+                () => {
+                    if (isLoading) {
+                        return (
+                            <div className='d-flex align-items-center justify-content-center h-100'>
+                                <Loader />
+                            </div>
+                        );
+                    } else {
+                        return (
+                            <div className='album'>
+                                <div 
+                                    style={{backgroundImage: 'var(--bs-gradient)'}}
+                                    className='d-flex d-md-block justify-content-center bg-primary rounded border-bottom-0 p-4 p-md-5 mb-3'
+                                >
+                                    <h1 className='text-white fw-normal'>Our latest articles</h1>
+                                </div>
+                                <div className='row row-cols-1 row-cols-md-3 g-3'>
+                                    {
+                                        articles.map((item) => {
+                                            return (
+                                                <ArticleCard 
+                                                    key={item.id} 
+                                                    {...item} 
+                                                    navigateArticleViewHandler={navigateArticleViewHandler} 
+                                                    navigateArticleEditHandler={navigateArticleEditHandler}
+                                                    deleteArticle={deleteArticle} 
+                                                />
+                                            );
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        );
+                    }
+                }
+            )()}
+        </>
+    );
+}
+
+export default Articles;
