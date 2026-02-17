f919bb4013594c10354fa917fcc9b7bbe8dc5956
AkobirCoder <akobir0308@gmail.com>
2026-02-17 17:17:07 +0500

    ./src/components/main/navbar-link.jsx was moved to main

---
+++ src/components/main/navbar-link.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavbarLink = ({activeNav, title, path, handleClick}) => {
+    return (
+        <>
+            <NavLink
+                to={path}
+                className={({isActive}) => `
+                    text-dark 
+                    text-decoration-none 
+                    py-2 px-3 
+                    rounded-top 
+                    nav-link-item
+                    ${isActive ? 'custom-nav-link' : ''}
+                `}
+                onClick={() => handleClick(title)}
+            >
+                {title}
+            </NavLink>
+        </>
+    );
+}
+
+export default NavbarLink;
