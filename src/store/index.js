fefaf58280e913f47595b8a454021c1e4bb79783
AkobirCoder <akobir0308@gmail.com>
2026-02-17 23:15:42 +0500

    ./src/store/index.js was updated

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
-import {configureStore} from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import AuthReducer from '../slice/auth';
 import ArticleReducer from '../slice/article';
+import ProfileReducer from '../slice/profile';
 import ProfileExtraReducer from '../slice/profileExtra';
 
 export default configureStore({
     reducer: {
         auth: AuthReducer,
         article: ArticleReducer,
+        profile: ProfileReducer,
         profileExtra: ProfileExtraReducer,
     },
     devTools: process.env.NODE_ENV !== 'production',
