d76eff3f175af8d93f4c6f77ae290e4035240260
AkobirCoder <akobir0308@gmail.com>
2026-02-17 17:17:41 +0500

    ./src/components/main/register.jsx was moved to main

---
+++ src/components/main/register.jsx
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { FormLogo } from '../assets/index';
+import { Input, registerInputProps } from '../../ui/index';
+import { 
+    signUserFailure, 
+    signUserStart, 
+    signUserSuccess 
+} from '../../slice/auth';
+import AuthService from '../../service/auth';
+import { ValidationError } from '../index';
+
+const Register = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+    });
+
+    const changeHandlerInput = (event) => {
+        const {name, value} = event.target;
+        
+        setFormData((prevState) => {
+            return {...prevState, [name]: value}
+        });
+    }
+
+    // const isFormValid = formData.username.trim() !== '' && formData.email.trim() !== '' && formData.password.trim() !== '';
+
+    const dispatch = useDispatch();
+
+    const {isLoading, loggedIn} = useSelector(state => state.auth);
+
+    const navigate = useNavigate();
+
+    const registerHandler = async (event) => {
+        event.preventDefault();
+
+        dispatch(signUserStart());
+        
+        const {username, email, password} = formData;
+        const user = {username, email, password}
+
+        try {
+            const response = await AuthService.userRegister(user);
+
+            // console.log(response);
+
+            // console.log(user);
+
+            navigate('/articles');
+
+            dispatch(signUserSuccess(response.user));
+        } catch (error) {
+            // console.log(error.response.data.errors);
+            dispatch(signUserFailure(error.response.data.errors));
+        }
+        
+        setFormData(() => {
+            return {
+                username: '',
+                email: '',
+                password: '',
+            }
+        });
+    }
+
+    useEffect(() => {
+        if (loggedIn) {
+            navigate('/articles');
+        }
+    }, [loggedIn, navigate]);
+
+    return (
+        <div className='row d-flex justify-content-center align-items-center' style={{minHeight: '100%'}}>
+            <main className='form-signin col-11 col-md-4'>
+                <form className='text-center mt-md-0' onSubmit={registerHandler}>
+                    <img className='mb-4' src={FormLogo} alt="Form logo" />
+                    <h1 className='h3 mb-3 fw-normal'>Please sign up</h1>
+
+                    <ValidationError /> {/* Validation to'g'irlanishi kerak */}
+
+                    {
+                        registerInputProps.map((inputProp) => {
+                            return (
+                                <Input
+                                    mark={'sign-up-form-input'}
+                                    key={inputProp.id}
+                                    {...inputProp}
+                                    value={formData[inputProp.name]}
+                                    changeHandlerInput={changeHandlerInput}
+                                />
+                            );
+                        })
+                    }
+
+                    <button 
+                        type='submit' 
+                        className='w-100 btn btn-lg btn-primary mt-2 mt-md-3'
+                        // disabled={!isFormValid || isLoading}
+                        disabled={isLoading}
+                    >
+                        {
+                            (() => {
+                                if (isLoading) {
+                                    return 'Loading...';
+                                } else {
+                                    return 'Sign up';
+                                }
+                            })()
+                        }
+                    </button>
+                    <p className='mt-3 mb-3 text-muted'>
+                        If you have an account, You must login by one click
+                        <span className='ms-2'> 
+                            <Link to={'/login'}>Sign in</Link>
+                        </span>
+                    </p>
+                    <p className='mt-3 mt-md-5 mb-3 text-muted'>© 2025-2026</p>
+                </form>
+            </main>
+        </div>
+    );
+}
+
+export default Register;
