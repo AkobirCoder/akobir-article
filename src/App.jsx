189166eed6faea878c03aed43b8b792f7444a327
AkobirCoder <akobir0308@gmail.com>
2026-02-17 17:08:08 +0500

    ./src/App.jsx was updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { 
     AboutUs, 
