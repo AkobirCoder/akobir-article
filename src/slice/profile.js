534e6a4082a0cc40697be90e8e70550243bf63da
AkobirCoder <akobir0308@gmail.com>
2026-02-17 23:15:03 +0500

    ./src/slice/profile.js was added

---
+++ src/slice/profile.js
@@ -0,0 +1,70 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    isLoading: false,
+    followLoading: false,
+    profile: null,
+    error: null,
+}
+
+export const profileSlice = createSlice({
+    name: 'profile',
+    initialState: initialState,
+    reducers: {
+        getProfileStart: (state) => {
+            state.isLoading = true;
+        },
+        getProfileSuccess: (state, action) => {
+            state.isLoading = false;
+            state.profile = action.payload;
+        },
+        getProfileFailure: (state, action) => {
+            state.isLoading = false;
+            state.error = action.payload;
+        },
+
+        postFollowProfileStart: (state) => {
+            state.isLoading = true;
+            state.followLoading = true;
+        },
+        postFollowProfileSuccess: (state, action) => {
+            state.isLoading = false;
+            state.followLoading = false;
+            state.profile = action.payload;
+        },
+        postFollowProfileFailure: (state, action) => {
+            state.isLoading = false;
+            state.followLoading = false;
+            state.error = action.payload;
+        },
+
+        deleteFollowProfileStart: (state) => {
+            state.isLoading = true;
+            state.followLoading = true;
+        },
+        deleteFollowProfileSuccess: (state, action) => {
+            state.isLoading = false;
+            state.followLoading = false;
+            state.profile = action.payload;
+        },
+        deleteFollowProfileFailure: (state, action) => {
+            state.isLoading = false;
+            state.followLoading = false;
+            state.error = action.payload;
+        }
+    }
+});
+
+export const {
+    getProfileStart,
+    getProfileSuccess,
+    getProfileFailure,
+    postFollowProfileStart,
+    postFollowProfileSuccess,
+    postFollowProfileFailure,
+    deleteFollowProfileStart,
+    deleteFollowProfileSuccess,
+    deleteFollowProfileFailure
+} = profileSlice.actions;
+
+export default profileSlice.reducer;
