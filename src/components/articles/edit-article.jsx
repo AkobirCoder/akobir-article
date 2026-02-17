39798593af1a18e4e5cd73550b509e5c6b6441ec
AkobirCoder <akobir0308@gmail.com>
2026-02-17 17:14:41 +0500

    ./src/components/articles/edit-article.jsx was moved to articles

---
+++ src/components/articles/edit-article.jsx
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import { 
+    getArticleDetailFailure, 
+    getArticleDetailStart, 
+    getArticleDetailSuccess, 
+    putArticleFailure, 
+    putArticleStart, 
+    putArticleSuccess 
+} from '../../slice/article';
+import ArticleService from '../../service/article';
+import { ArticleForm } from '../../ui/index';
+import { getItem } from '../../helpers/persistance-storage';
+
+const EditArticle = () => {
+    const [formData, setFormData] = useState({
+        title: '',
+        description: '',
+        body: '',
+    });
+
+    const changeHandlerInput = (event) => {
+        const {name, value} = event.target;
+
+        setFormData((prevState) => {
+            return {...prevState, [name]: value}
+        });
+    }
+
+    const {slug} = useParams();
+
+    const dispatch = useDispatch();
+
+    const {user} = useSelector((state) => state.auth);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const getArticleDetail = async () => {
+            dispatch(getArticleDetailStart());
+        
+            try {
+                const response = await ArticleService.getArticleDetail(slug); // bunday qilishdan asosiy maqsad articleDetaildagi ma'lumotlarni olib formaga qiymat sifatida berish va bu ma'lumotlar har bir article slugi orqali bo'ladi.
+
+                if (response.article.author.username === user.username) {
+                    setFormData(() => {
+                        return {
+                            title: response.article.title,
+                            description: response.article.description,
+                            body: response.article.body,
+                        }
+                    });
+
+                    dispatch(getArticleDetailSuccess(response.article));
+                } else {
+                    navigate('/');
+                }
+            } catch (error) {
+                dispatch(getArticleDetailFailure());
+            }
+        }
+
+        if (!user?.username) return;
+
+        const token = getItem('token');
+        
+        if (!token) {
+            navigate('/login');
+            
+            return;
+        }
+
+        getArticleDetail();
+    }, [slug, dispatch, user, navigate]);
+
+    const formSubmit = async (event) => {
+        event.preventDefault();
+
+        dispatch(putArticleStart());
+
+        const {title, description, body} = formData;
+
+        const article = {title, description, body}
+
+        try {
+            const response = await ArticleService.editArticle(slug, article);
+
+            dispatch(putArticleSuccess(response));
+
+            navigate('/articles');
+        } catch (error) {
+            dispatch(putArticleFailure());
+        }
+    }
+
+    const btnName = 'Edit';
+ 
+    return (
+        <div className='row d-flex align-items-center justify-content-center' style={{minHeight: '100%'}}>
+            <div className='col-11 col-md-8'>
+                <h1 className='fs-2 text-center'>Edit article</h1>
+                <ArticleForm
+                    formData={formData} 
+                    changeHandlerInput={changeHandlerInput} 
+                    formSubmit={formSubmit} 
+                    btnName={btnName}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default EditArticle;
