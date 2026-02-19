f9f70548d1f3b3818418e844d1e08e111d28a2e6
AkobirCoder <akobir0308@gmail.com>
2026-02-19 22:44:25 +0500

    ./src/components/user/user-followers.jsx was added

---
+++ src/components/user/user-followers.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { UserFollowersItem } from '../index'; 
+
+const UserFollowers = () => {
+    // articles ni qo'llash kerak.
+
+    return (
+        <div className='row'>
+            <UserFollowersItem />
+        </div>
+    );
+}
+
+export default UserFollowers;
