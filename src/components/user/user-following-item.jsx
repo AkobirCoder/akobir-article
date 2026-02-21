a9c5f4e817eb862dc317bf4b5e37249aef23e358
AkobirCoder <akobir0308@gmail.com>
2026-02-22 01:52:42 +0500

    ./src/components/user/user-following-item.jsx was added

---
+++ src/components/user/user-following-item.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserFollowingItem = ({username}) => {
+    return (
+        <div className='col-12 col-md-7'>
+            {username}
+        </div>
+    );
+}
+
+export default UserFollowingItem;
