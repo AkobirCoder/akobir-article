acb5c05f8d2d719571fc28ba63f75faa7503d754
AkobirCoder <akobir0308@gmail.com>
2026-02-22 23:31:39 +0500

    ./src/components/user/user-following.jsx was updated

--- src/components/user/user-following.jsx
+++ src/components/user/user-following.jsx
@@ -1,18 +1,30 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { UserFollowingItem } from '../index'; 
-// import { getArticlesFailure, getArticlesStart, getArticlesSuccess, } from '../../slice/article';
 import ArticleService from '../../service/article';
-import { getFollowingUserFailure, getFollowingUserStart, getFollowingUserSuccess } from '../../slice/following';
+import { 
+    getFollowingUserFailure, 
+    getFollowingUserStart, 
+    getFollowingUserSuccess 
+} from '../../slice/following';
+import { getItem } from '../../helpers/persistance-storage';
+import { Loader } from '../../ui/index';
 
 const UserFollowing = () => {
     const dispatch = useDispatch();
 
-    // const {articles} = useSelector((state) => state.article);
+    const {users, isLoading} = useSelector((state) => state.following);
 
-    const {users} = useSelector((state) => state.following);
+    const navigate = useNavigate();
 
     useEffect(() => {
+        const token = getItem('token');
+
+        if (!token) {
+            navigate('/');
+        }
+
         const getUserFollowing = async () => {
             dispatch(getFollowingUserStart());
 
@@ -21,26 +33,46 @@ const UserFollowing = () => {
 
                 dispatch(getFollowingUserSuccess(response));
             } catch (error) {
-                dispatch(getFollowingUserFailure);
+                dispatch(getFollowingUserFailure(error.response.data.errors));
             }
         }
 
         getUserFollowing();
-    }, [dispatch]);
+    }, [navigate, dispatch]);
 
     return (
-        <div className='row'>
-            {
-                users.map((userItem) => {
-                    return (
-                        <UserFollowingItem 
-                            key={userItem.username} 
-                            {...userItem}
-                        />
-                    );
-                })
-            }
-        </div>
+        <>
+            {(
+                () => {
+                    if (isLoading) {
+                        return (
+                            <div className='d-flex align-items-center justify-content-center h-100'>
+                                <Loader />
+                            </div>
+                        );
+                    } else {
+                        return (
+                            <div className='overflow-auto bg-light border rounded p-3' style={{maxHeight: '100%'}}>
+                                <div className='row g-0 d-flex justify-content-center'>
+                                    {
+                                        users.map((userItem, index) => {
+                                            return (
+                                                <UserFollowingItem
+                                                    key={userItem.username}
+                                                    {...userItem}
+                                                    index={index}
+                                                    users={users}
+                                                />
+                                            );
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        );
+                    }
+                }
+            )()}
+        </>
     );
 }
 
