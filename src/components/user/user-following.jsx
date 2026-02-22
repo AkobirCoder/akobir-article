0989fa4b99c815dbdae987a7e03aab6c37ba44ff
AkobirCoder <akobir0308@gmail.com>
2026-02-22 17:05:50 +0500

    ./src/components/user/user-following.jsx was updated

--- src/components/user/user-following.jsx
+++ src/components/user/user-following.jsx
@@ -1,24 +1,27 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { UserFollowingItem } from '../index'; 
-import { getArticlesFailure, getArticlesStart, getArticlesSuccess, } from '../../slice/article';
+// import { getArticlesFailure, getArticlesStart, getArticlesSuccess, } from '../../slice/article';
 import ArticleService from '../../service/article';
+import { getFollowingUserFailure, getFollowingUserStart, getFollowingUserSuccess } from '../../slice/following';
 
 const UserFollowing = () => {
     const dispatch = useDispatch();
 
-    const {articles} = useSelector((state) => state.article);
+    // const {articles} = useSelector((state) => state.article);
+
+    const {users} = useSelector((state) => state.following);
 
     useEffect(() => {
         const getUserFollowing = async () => {
-            dispatch(getArticlesStart());
+            dispatch(getFollowingUserStart());
 
             try {
-                const response = await ArticleService.getFollowingArticles();
+                const response = await ArticleService.getFollowingUsers();
 
-                dispatch(getArticlesSuccess(response));
+                dispatch(getFollowingUserSuccess(response));
             } catch (error) {
-                dispatch(getArticlesFailure());
+                dispatch(getFollowingUserFailure);
             }
         }
 
@@ -28,11 +31,11 @@ const UserFollowing = () => {
     return (
         <div className='row'>
             {
-                articles?.map((articleItem) => {
+                users.map((userItem) => {
                     return (
                         <UserFollowingItem 
-                            key={articleItem.username} 
-                            {...articleItem}
+                            key={userItem.username} 
+                            {...userItem}
                         />
                     );
                 })
