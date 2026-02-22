5f2170ee049068c767c44314c18f24812fa34116
AkobirCoder <akobir0308@gmail.com>
2026-02-22 17:07:03 +0500

    ./src/store/index.js was updated

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import AuthReducer from '../slice/auth';
 import ArticleReducer from '../slice/article';
 import ProfileReducer from '../slice/profile';
+import FollowingReducer from '../slice/following';
 import ProfileExtraReducer from '../slice/profileExtra';
 
 export default configureStore({
@@ -9,6 +10,7 @@ export default configureStore({
         auth: AuthReducer,
         article: ArticleReducer,
         profile: ProfileReducer,
+        following: FollowingReducer,
         profileExtra: ProfileExtraReducer,
     },
     devTools: process.env.NODE_ENV !== 'production',
