e1c220033ba541fea28ccc2ec4b23d83db9c45bc
AkobirCoder <akobir0308@gmail.com>
2026-02-24 16:11:47 +0500

    ./src/service/article.js was updated

--- src/service/article.js
+++ src/service/article.js
@@ -8,11 +8,13 @@ const ArticleService = {
 
         return response.data;
     },
+
     async getArticleDetail(slug) {
         const response = await axios.get(`/articles/${slug}`);
 
         return response.data;
     },
+
     async postArticle(article) {
         const response = await axios.post('/articles', {article});
 
@@ -28,6 +30,7 @@ const ArticleService = {
 
         return response.data;
     },
+
     async getFollowingUsers() {
         const response = await axios.get('/articles?limit=100');
 
@@ -42,6 +45,39 @@ const ArticleService = {
         });
 
         return Array.from(map.values());
+    },
+
+    async postArticleFavorite(slug) {
+        const response = await axios.post(`/articles/${slug}/favorite`);
+
+        return response.data;
+    },
+    async deleteArticleFavorite(slug) {
+        const response = await axios.delete(`/articles/${slug}/favorite`);
+
+        return response.data;
+    },
+
+    async getArticleComment(slug) {
+        const response = await axios.get(`/articles/${slug}/comments`);
+
+        return response.data;
+    },
+    async postArticleComment(slug, comment) {
+        const response = await axios.post(`/articles/${slug}/comments`, {comment});
+
+        return response.data;
+    },
+    async deleteArticleComment(slug, commentId) {
+        const response = await axios.delete(`/articles/${slug}/comments/${commentId}`);
+
+        return response.data;
+    },
+
+    async getFeedArticles() {
+        const response = await axios.get('/articles/feed');
+
+        return response.data;
     }
 }
 
