7ea6c444c0a197bf4814b6e6ffa12244518c96a4
AkobirCoder <akobir0308@gmail.com>
2026-02-24 19:38:13 +0500

    ./src/App.jsx was updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { 
     AboutUs, 
     Appearance, 
-    ArticleComment, 
+    // ArticleComment, 
     ArticleDetail, 
     ArticleFeed, 
     Articles, 
