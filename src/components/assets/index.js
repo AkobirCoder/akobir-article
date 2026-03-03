4bd666c5d39db240e3892697ecd1b7616518cc04
AkobirCoder <akobir0308@gmail.com>
2026-03-03 23:22:44 +0500

    ./src/components/assets/index.js was updated

--- src/components/assets/index.js
+++ src/components/assets/index.js
@@ -1,3 +1,4 @@
 export {default as Logo} from './logo.svg';
 export {default as FormLogo} from './form-logo.svg';
 export {default as HomeImage} from './home.png';
+export {default as AuthorImage} from './akobir-color.png';
